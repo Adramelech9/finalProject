cd43c2c11cd803010ba16b52d64a0165cdd7992f
Adramelech9 <lapitan31343@gmail.com>
2021-09-13 18:21:50 +0300

    up

--- src/main/java/com/itransition/training/task04/Users/UsersApplication.java
+++ src/main/java/com/itransition/training/task04/Users/UsersApplication.java
@@ -26,6 +26,7 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
@@ -38,7 +39,7 @@ import java.util.Map;
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class UsersApplication extends WebSecurityConfigurerAdapter {
 	private final UserRepository userRepository;
 
