70e34e1543fb3101314e7eea4d3d163398ccd652
Adramelech9 <lapitan31343@gmail.com>
2021-09-19 21:53:25 +0300

    baсk

--- src/main/resources/db/migration/V1__createQueries.sql
+++
@@ -1,9 +0,0 @@
-create sequence hibernate_sequence start 1 increment 1;
-
-create table table_users (id varchar(255) not null,
-first_entry timestamp,
-is_active boolean not null,
-last_entry timestamp,
-social_network varchar(255),
-user_name varchar(255),
-primary key (id));
