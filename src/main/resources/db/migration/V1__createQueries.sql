923067b57fa1ef9459f7389a8738c450b1ded502
Adramelech9 <lapitan31343@gmail.com>
2021-09-19 21:26:11 +0300

    add migration

---
+++ src/main/resources/db/migration/V1__createQueries.sql
@@ -0,0 +1,7 @@
+create table table_users (id varchar(255) not null,
+first_entry timestamp,
+is_active boolean not null,
+last_entry timestamp,
+social_network varchar(255),
+user_name varchar(255),
+primary key (id));
