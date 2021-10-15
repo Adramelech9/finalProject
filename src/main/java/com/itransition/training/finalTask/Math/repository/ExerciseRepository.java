b6a3b396f0dc6839565d40f460efd88b047fa262
Adramelech9 <lapitan31343@gmail.com>
2021-10-15 15:50:06 +0300

    add distinct

--- src/main/java/com/itransition/training/finalTask/Math/repository/ExerciseRepository.java
+++ src/main/java/com/itransition/training/finalTask/Math/repository/ExerciseRepository.java
@@ -12,7 +12,7 @@ public interface ExerciseRepository extends CrudRepository<Exercises, Long> {
 
     Page<Exercises> findAll(Pageable pageable);
 
-    @Query("select e from Exercises e join Comment c on e.id = c.idCommentExercise where concat(" +
+    @Query("select distinct e from Exercises e join Comment c on e.id = c.idCommentExercise where concat(" +
             "e.name, e.condition, e.tags, e.theme, e.author.username, c.text) " +
             "like %?1% ")
     List<Exercises> search(String keyword);
